db03b47a7d28f487ba22fa7d91ef87782d6e9399
fr1x3 <gathufx@gmail.com>
2021-01-19 23:54:39 +0300

    initial commit

---
+++ app/src/main/java/com/fr1x3/onlinenewsapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fr1x3.onlinenewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fr1x3.onlinenewsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
